6f2f0c192dc606aa8c04abff7a776cbca49ddbfa
AJ-Walker <akj441@gmail.com>
2022-07-05 11:44:10 +0530

    Added gitignore

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!jx&2wjaxaq9=jb!=&ee)k9v644nj8+l7%q)r7=)c(9f!tl)2y'
+# SECRET_KEY = 'django-insecure-!jx&2wjaxaq9=jb!=&ee)k9v644nj8+l7%q)r7=)c(9f!tl)2y'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!jx&2wjaxaq9=jb!=&ee)k9v644nj8+l7%q)r7=)c(9f!tl)2y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
